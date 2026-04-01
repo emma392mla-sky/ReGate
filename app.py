2349dda14ed6cfb76e7132d567a58ef3b00c5191
emma392mla-sky <emma392mla@gmail.com>
2026-04-01 05:59:13 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request,jsonify
+
+app = Flask("__name__")
+
+@app.route("/")
+def home():
+    return render_template("index.html", name="mark")
+
+@app.route("/run", methods=["POST"])
+def call():
+    name = request.json.get("name")
+    phone = request.json.get("phone")
+
+    return jsonify({
+        "name":"new_"+name,
+        "phone":"new_"+phone
+        })
+
+if __name__ == "__main__":
+    app.run(debug=True)
