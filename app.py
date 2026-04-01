40f98567060d162f4637a7cda336d6c974a6ecb6
emma392mla-sky <emma392mla@gmail.com>
2026-04-01 17:28:50 +0200

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request,jsonify
+import os
 
 app = Flask("__name__")
 
@@ -17,4 +18,4 @@ def call():
         })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
